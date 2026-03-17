86ba8f053daf7a3c4fd514f3400ade5dfdf6f232
Shubham Sharma <shubhamsharma64337@gmail.com>
2026-03-17 20:30:38 +0530

    Added developer nametag in loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,10 @@
 function Loader(props) {
   return (
-    <div className={`fixed ${!(props.loading)?'hidden':''} justify-center items-center flex left-0 top-0 bg-slate-100 opacity-50 w-full h-full`}>
+    <div className={`fixed ${!(props.loading)?'hidden':''} justify-center items-center flex flex-col left-0 top-0 bg-slate-100 opacity-50 w-full h-full`}>
         <span className={`${!(props.loading)?'hidden':''} w-2 h-2 border-4 border-t-slate-100 border-purple-900 animate-spin rounded-full opacity-100 px-5 py-5`}></span>
+        <span className="italic hidden">Developed by ShubhamSharma64337</span>
     </div>
+    
   )
 }
 
